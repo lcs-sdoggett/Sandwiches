889527fb06cc84e58f38224a6d7453b58e423868
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-20 20:13:27 -0500

    Content view created and worked on. SandwichDetail created.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    var sandwiches: [Sandwich] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach (sandwiches) { sandwich in
+                    SandwichCell(sandwich: sandwich)
+                }
+                
+                HStack {
+                    Spacer()
+                    Text("\(sandwiches.count) Sandwiches")
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+            }
+            .navigationTitle("Sandwiches")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sandwiches: testData)
+    }
+}
+
+struct SandwichCell: View {
+    var sandwich: Sandwich
+    var body: some View {
+        NavigationLink(destination: Text(sandwich.name)) {
+            
+            Image(sandwich.thumbnailName)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(sandwich.name)
+                Text("\(sandwich.ingredientCount) ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
---
+++ Shared/SandwichDetail.swift
@@ -0,0 +1,20 @@
+//
+//  SandwichDetail.swift
+//  Sandwiches
+//
+//  Created by Doggett, Scott on 2021-01-20.
+//
+
+import SwiftUI
+
+struct SandwichDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SandwichDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        SandwichDetail()
+    }
+}
